a6fa62ab09d11c9731cb13f8224f6f08032babea
ya_boiko <acc.v.boiko@gmail.com>
2023-06-21 23:52:15 +0300

    money bot v0

---
+++ main.py
@@ -0,0 +1,63 @@
+from settings import TOKEN, USERS, DB_NAME
+from datetime import datetime
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ParseMode
+from Db import Db
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+db = Db(DB_NAME)
+
+db.execute("""
+    CREATE TABLE IF NOT EXISTS money_notes(
+        note_date TEXT DEFAULT '',
+        tg_user_id TEXT DEFAULT '',
+        note TEXT DEFAULT ''
+    );
+""")
+
+
+@dp.message_handler(commands=['start'])
+async def start_menu(msg: types.Message):
+    await msg.answer(
+        text=f"money money money",
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
+@dp.message_handler(content_types=['text'])
+async def get_text_message(msg: types.Message):
+    words_list = msg.text.strip().split()
+    if msg.from_user.id not in USERS:
+        await msg.answer(
+            text=f"Ты че пёс охуел 🖕",
+            parse_mode=ParseMode.MARKDOWN
+        )
+        return
+
+    for i in words_list:
+        if i.isnumeric():
+            db.insert(
+                query="""
+                    INSERT INTO money_notes (
+                        note_date,
+                        tg_user_id,
+                        note
+                    ) VALUES (?, ?, ?);
+                """,
+                data=[(
+                    datetime.today().strftime("%d.%m.%Y %H:%M:%S"),
+                    msg.from_user.id,
+                    msg.text.strip()
+                )]
+            )
+            await msg.answer(
+                text=f"Спасибки, записаль ✍️",
+                parse_mode=ParseMode.MARKDOWN
+            )
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+dotenv_path = Path(".env")
+load_dotenv(dotenv_path=dotenv_path)
+
+TOKEN = os.getenv("TOKEN")
+USERS = os.getenv("USERS")
+DB_NAME = os.getenv("DB_NAME")
+pass
